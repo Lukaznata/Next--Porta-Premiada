18bd5a238c81aec36c3a6abc13c2bfd5d52a56bc
Lucas Nathan Tonin <lucas18tonin@gmail.com>
2024-07-02 20:33:26 -0300

    primeiro commit

---
+++ components/Porta.tsx
@@ -0,0 +1,29 @@
+// STYLES
+import styles from "@/styles/Porta.module.css";
+
+// MODELS
+import PortaModel from "@/model/porta";
+
+interface portaProps {
+  value: PortaModel;
+}
+
+export default function Porta(props: portaProps) {
+  const porta = props.value;
+  const selecionada = porta.selecionada ? styles.selecionada : "";
+
+  return (
+    <>
+      <div className={styles.area}>
+        <div className={`${styles.estrutura} ${selecionada}`}>
+          <div className={styles.porta}>
+            <div className={styles.numero}>{porta.numero}</div>
+            <div className={styles.macaneta}></div>
+          </div>
+        </div>
+
+        <div className={styles.chao}></div>
+      </div>
+    </>
+  );
+}
